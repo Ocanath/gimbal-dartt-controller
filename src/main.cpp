02549b03f4a52aa757d352c839ec02572f4e3a18
Jesse Cornman <ocanath@gmail.com>
2025-09-21 23:05:36 -0700

    dartt inclusion, and I am realizing that me building a bunch of extraneous executables is really annoying so i'm going to clean that up next

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "serial.h"
 #include "dartt.h"
+#include "cobs.h"
 
 
 int main()
@@ -24,11 +25,25 @@ int main()
         .len = 0
     };
     int rc = dartt_create_write_frame(&wmsg, TYPE_SERIAL_MESSAGE, &uart_tx);
+    cobs_buf_t cobs_tx_buf = 
+    {
+        .buf = uart_tx_buf,
+        .size = sizeof(uart_tx_buf),
+        .length = uart_tx.len,
+        .encoded_state = COBS_DECODED
+    };
+    rc = cobs_encode_single_buffer(&cobs_tx_buf);
+    if(rc != COBS_SUCCESS)
+    {
+        printf("Cobs usage error %d\n", rc);
+        return rc;
+    }
+
 
     if(serial.autoconnect(2000000))
     {
         printf("Connection success\n");
-		serial.write(uart_tx.buf, uart_tx.len);
+		serial.write(uart_tx.buf, (int)uart_tx.len);
 		uart_tx.len = 0;
         return 0;
     }
